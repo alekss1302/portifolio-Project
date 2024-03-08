30231d4f6e8837841ab12d4c0b53b8f2277761e6
Alekss Dzirvinskis <alekss13002@gmail.com>
2024-03-08 18:19:32 +0000

    Blog functionality and polish

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -8,8 +8,8 @@ from jobs.views import home
 from django.urls import include, path
 
 urlpatterns = [
-    # path('', views.home, name='home'),
     path('blog/', include('blog.urls')),  # Ensure 'blog.urls' is correct
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
